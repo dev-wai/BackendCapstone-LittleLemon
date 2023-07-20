9fee733beedfaa97b26a4f5013e59c24df35e0ab
ShahandFahad <shahandfahadmad@gmail.com>
2023-07-20 12:28:48 +0500

    ReadingExercise: Setting up the models

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,8 +7,14 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField()
     booking_date = models.DateField()
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Menu(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
