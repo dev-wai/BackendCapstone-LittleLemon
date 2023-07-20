08d8a406d1848eeb95cf4957045bed502bd43f98
ShahandFahad <shahandfahadmad@gmail.com>
2023-07-20 12:23:34 +0500

    Changing models structure for Booking and Menu

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True)
     no_of_guests = models.IntegerField()
     booking_date = models.DateField()
 
 
 class Menu(models.Model):
-    title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=2, decimal_places=2)
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     inventory = models.IntegerField()
