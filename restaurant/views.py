4e85007ced1f4af00151b1b323fd2f92e3d98c49
ShahandFahad <shahandfahadmad@gmail.com>
2023-07-20 15:46:08 +0500

    Exercise: Set up the menu API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Menu
+from .serializers import MenuSerializer
+
+from rest_framework import generics
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+# Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
